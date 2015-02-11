7f2e6bdcc8e410d7f72f88f6dc7e26ff3cb62ebf
chakalakka <a.s.taudt@umcg.nl>
2015-02-11 13:12:50 +0100

    fixed bug in loadHmmsFromFiles

--- R/loadHmmsFromFiles.R
+++ R/loadHmmsFromFiles.R
@@ -1,42 +1,38 @@
-loadHmmsFromFiles <- function(hmm.list) {
+loadHmmsFromFiles <- function(filenames) {
 
 	## Intercept user input
-	if (check.univariate.modellist(hmm.list)!=0) {
+	if (check.univariate.modellist(filenames)!=0) {
 		message("loading univariate HMMs from files ...", appendLF=F)
 		ptm <- proc.time()
-		mlist <- NULL
-		for (modelfile in hmm.list) {
+		mlist <- list()
+		for (modelfile in filenames) {
 			mlist[[length(mlist)+1]] <- get(load(modelfile))
 		}
-		hmm.list <- mlist
-		remove(mlist)
 		time <- proc.time() - ptm
 		message(" ",round(time[3],2),"s")
-		if (check.univariate.modellist(hmm.list)!=0) stop("argument 'hmm.list' expects a list of univariate hmms or a list of files that contain univariate hmms")
+		if (check.univariate.modellist(mlist)!=0) stop("argument 'filenames' expects a list of univariate hmms or a list of files that contain univariate hmms")
 	}
 	
-	return(hmm.list)
+	return(filenames)
 
 }
 
-loadMultiHmmsFromFiles <- function(hmm.list) {
+loadMultiHmmsFromFiles <- function(filenames) {
 
 	## Intercept user input
-	if (check.multivariate.modellist(hmm.list)!=0) {
+	if (check.multivariate.modellist(filenames)!=0) {
 		message("loading multivariate HMMs from files ...", appendLF=F)
 		ptm <- proc.time()
-		mlist <- NULL
-		for (modelfile in hmm.list) {
+		mlist <- list()
+		for (modelfile in filenames) {
 			mlist[[length(mlist)+1]] <- get(load(modelfile))
 		}
-		hmm.list <- mlist
-		remove(mlist)
 		time <- proc.time() - ptm
 		message(" ",round(time[3],2),"s")
-		if (check.multivariate.modellist(hmm.list)!=0) stop("argument 'hmm.list' expects a list of multivariate hmms or a list of files that contain multivariate hmms")
+		if (check.multivariate.modellist(mlist)!=0) stop("argument 'filenames' expects a list of multivariate hmms or a list of files that contain multivariate hmms")
 	}
 	
-	return(hmm.list)
+	return(filenames)
 
 }
 
