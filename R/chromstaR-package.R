8185a8dc7d546dcb111ac6aa02d3e615a2b15d2c
chakalakka <a.s.taudt@umcg.nl>
2015-11-16 08:30:02 +0100

    added vignette

---
+++ R/chromstaR-package.R
@@ -0,0 +1,13 @@
+
+
+#' Combinatorial and differential ChIP-seq analysis
+#'
+#' This package implements functions for the combinatorial and differential analysis of ChIP-seq data.
+#'
+#' Please read \href{../doc/chromstaR.pdf}{The chromstaR user's guide} for a quick introduction.
+#'
+#' @author Aaron Taudt, Maria Colome-Tatche, Matthias Heinig, Minh Anh Nguyen
+#' @docType package
+#' @name chromstaR-package
+#' @aliases chromstaR
+NULL
