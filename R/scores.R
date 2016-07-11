b465d888e3ac6e59ac070eff50c7ef59a0ec91fb
chakalakka <a.s.taudt@umcg.nl>
2016-07-11 07:44:10 +0200

    added arbitrary function to differentialScores

--- R/scores.R
+++ R/scores.R
@@ -2,40 +2,42 @@
 #'
 #' Various scores used in \code{\link{chromstaR}}.
 #'
-#' @param posteriors A matrix with posterior probabilities.
+#' @param mat A matrix with posterior probabilities or read counts. Column names must correspond to the ID entries in \code{info}.
 #' @param info An \code{\link{experiment.table}} with additional column 'ID'.
+#' @param FUN A function to compute the score with.
 #' @name scores
 #' @return A numeric vector.
 #' @author Aaron Taudt
 NULL
 
 #' @describeIn scores Maximum differential score. Values are between 0 and 1. A value of 1 means that at least one mark is maximally different between conditions.
-differentialScoreMax <- function(posteriors, info) {
+differentialScoreMax <- function(mat, info, FUN='-') {
 
-    if (is.null(posteriors) | is.null(info)) {
+    if (is.null(mat) | is.null(info)) {
         return(NULL)
     }
-    differential.scores <- differentialScores(posteriors, info)
+    differential.scores <- differentialScores(mat, info, FUN)
     differential.score <- do.call(pmax, differential.scores)
     return(differential.score)
 }
 
 
 #' @describeIn scores Additive differential score. Values are between 0 and N, where N is the number of marks. A value around 1 means that approximately 1 mark is different, a value of 2 means that 2 marks are different etc.
-differentialScoreSum <- function(posteriors, info) {
+differentialScoreSum <- function(mat, info, FUN='-') {
 
-    if (is.null(posteriors) | is.null(info)) {
+    if (is.null(mat) | is.null(info)) {
         return(NULL)
     }
-    differential.scores <- differentialScores(posteriors, info)
+    differential.scores <- differentialScores(mat, info, FUN)
     differential.score <- rowSums(as.data.frame(differential.scores))
     return(differential.score)
 }
 
 
-differentialScores <- function(posteriors, info) {
+differentialScores <- function(mat, info, FUN='-') {
 
-    if (is.null(posteriors) | is.null(info)) {
+    FUN <- match.fun(FUN)
+    if (is.null(mat) | is.null(info)) {
         return(NULL)
     }
     info.rep <- split(info, paste0(info$mark, '-', info$condition))
@@ -48,13 +50,12 @@ differentialScores <- function(posteriors, info) {
     differential.scores <- list()
     for (mark in names(info.mark)) {
         if (nrow(info.mark[[mark]]) > 1) {
-            min.post <- do.call(pmin, as.list(as.data.frame(posteriors[,info.mark[[mark]]$ID])))
-            max.post <- do.call(pmax, as.list(as.data.frame(posteriors[,info.mark[[mark]]$ID])))
-            differential.scores[[mark]] <- max.post - min.post
+            min.post <- do.call(pmin, as.list(as.data.frame(mat[,info.mark[[mark]]$ID])))
+            max.post <- do.call(pmax, as.list(as.data.frame(mat[,info.mark[[mark]]$ID])))
+            differential.scores[[mark]] <- FUN(max.post, min.post)
         } else if (nrow(info.mark[[mark]]) == 1) {
-            differential.scores[[mark]] <- rep(0, nrow(posteriors))
+            differential.scores[[mark]] <- rep(0, nrow(mat))
         }
     }
     return(differential.scores)
 }
-
