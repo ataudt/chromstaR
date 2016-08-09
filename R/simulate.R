94dfd1f69e14b38ff90d447e4314cfd51bc7fa98
chakalakka <a.s.taudt@umcg.nl>
2016-08-09 11:18:15 +0200

    updated simulation functions

--- R/simulate.R
+++ R/simulate.R
@@ -1,5 +1,14 @@
+#' Simulate data
+#' 
+#' Simulate known states, read counts and read coordinates using a univariate Hidden Markov Model with three states ("zero-inflation", "unmodified" and "modified").
+#' 
+#' @param bins A \code{\link[GenomicRanges]{GRanges}} object for which reads will be simulated.
+#' @param transition A matrix with transition probabilities.
+#' @param emission A data.frame with emission distributions (see \code{\link{uniHMM}} entry 'distributions').
+#' @inheritParams simulateReads
+#' @return A \code{list} with entries $bins containing the simulated states and read count, $reads with simulated read coordinates and $transition and $emission.
 #' @importFrom stats runif rnbinom aggregate
-simulateUnivariate = function(bins, transition, emission, initial=1) {
+simulateUnivariate <- function(bins, transition, emission, fragLen=50) {
 
     # Calculate some variables
     numstates = ncol(transition)
@@ -14,21 +23,21 @@ simulateUnivariate = function(bins, transition, emission, initial=1) {
     # Integrate transition matrix by row and add -1 in front
     cumtransition = cbind(rep(-1,numstates), t(apply(transition, 1, cumsum)))
     # Generate the state vector by going through each state
-    message("Generating states from transition matrix...", appendLF=FALSE)
+    ptm <- startTimedMessage("Generating states from transition matrix ...")
     states = matrix(rep(NA,numstates*numbins), ncol=numstates)
     for (irow in 1:numstates) {
         states[,irow] = findInterval(rsample, cumtransition[irow,], rightmost.closed=TRUE)
     }
     statevec = rep(NA,numbins)
-    statevec[1] = initial
+    statevec[1] = 1
     for (ibin in 2:numbins) {
         statevec[ibin] = states[ibin,statevec[ibin-1]]
     }
-    message(" done")
+    stopTimedMessage(ptm)
 
     ## Make reads from state vector and distributions
     # Generate the read counts by drawing from distribution
-    message("Generating reads from emission parameters and states...", appendLF=FALSE)
+    ptm <- startTimedMessage("Generating read counts from emission parameters and states ...")
     reads = rep(NA, numbins)
     numbins.in.state = stats::aggregate(rep(1,length(statevec)), list(state=statevec), sum)
     reads[statevec==1] = 0
@@ -38,20 +47,63 @@ simulateUnivariate = function(bins, transition, emission, initial=1) {
     if (!is.na(numbins.in.state[3,'x'])) {
         reads[statevec==3] = stats::rnbinom(numbins.in.state[3,'x'], size=emission[3,'size'], prob=emission[3,'prob'])
     }
-    message(" done")
+    stopTimedMessage(ptm)
 
     ## Combine in GRanges
     bins.out <- bins
+    mcols(bins.out) <- NULL
     bins.out$counts <- reads
-    bins.out$state <- statevec
+    bins.out$state <- state.labels[statevec]
 
     # Return the output
     out = list(bins = bins.out,
-                transition = transition,
-                emission = emission
-                )
+               reads = simulateReads(bins.out),
+               transition = transition,
+               emission = emission
+               )
     return(out)
 
+}
+
+
+#' Simulate read coordinates
+#' 
+#' Simulate read coordinates using read counts as input.
+#' 
+#' @param bins A \code{\link[GenomicRanges]{GRanges}} with read counts.
+#' @param fragLen Length of the simulated read fragments.
+#' @return A \code{\link[GenomicRanges]{GRanges}} with read coordinates.
+simulateReads <- function(bins, fragLen=50) {
+    
+    ptm <- startTimedMessage("Generating read coordinates ...")
+    ## Do this for every chromosome
+    bins.splt <- split(bins, seqnames(bins))
+    reads <- GRangesList()
+    seqlevels(reads) <- seqlevels(bins)
+    seqlengths(reads) <- seqlengths(bins)
+    for (chrom in names(bins.splt)) {
+        bins.chrom <- bins.splt[[chrom]]
+        if (length(bins.chrom) > 0) {
+            total.reads <- sum(bins.chrom$counts)
+            binsize <- mean(width(bins.chrom))
+            ## For each read, get the start position
+            rlcounts <- rle(1)
+            rlcounts$lengths <- bins.chrom$counts
+            rlcounts$values <- as.numeric(start(bins.chrom))
+            starts <- inverse.rle(rlcounts)
+            ## For each read, simulate a random offset to the start of the bin
+            offsets <- round(runif(total.reads, min=0, max=binsize))
+            strand <- c('+','-')[round(runif(total.reads, min=1, max=2))]
+            ## Make read coordinates
+            reads[[chrom]] <- sort(GRanges(seqnames=chrom, ranges=IRanges(start=starts+offsets, end=starts+offsets+fragLen)))
+            strand(reads[[chrom]]) <- strand
+        }
+    }
+    reads <- unlist(reads, use.names=FALSE)
+    stopTimedMessage(ptm)
+    
+    return(reads)
+    
 }
 
 
