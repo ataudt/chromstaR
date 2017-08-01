466acb0a6b90b8d28379d8694452d530280cc8cd
chakalakka <a.s.taudt@umcg.nl>
2017-08-01 09:51:10 +0200

    no posteriorScores in segmentation

--- R/segmentation.R
+++ R/segmentation.R
@@ -12,10 +12,11 @@ multivariateSegmentation <- function(bins, column2collapseBy='state') {
     df <- as.data.frame(bins)
     ind.readcols <- grep('^counts', names(df))
     ind.postcols <- grep('^posteriors', names(df))
+    ind.postscorecols <- grep('^posteriorScores', names(df))
     ind.peakcols <- grep('^peakScores', names(df))
     ind.widthcol <- grep('width', names(df))
     ind.scorecol <- grep('differential.score', names(df))
-    red.df <- suppressMessages(collapseBins(df, column2collapseBy=column2collapseBy, columns2average=c(ind.scorecol), columns2drop=c(ind.readcols, ind.widthcol, ind.postcols)))
+    red.df <- suppressMessages(collapseBins(df, column2collapseBy=column2collapseBy, columns2average=c(ind.scorecol), columns2drop=c(ind.readcols, ind.widthcol, ind.postcols, ind.postscorecols)))
     names(red.df) <- sub('^mean.','', names(red.df))
     red.peakscores <- as.matrix(red.df[grep('^peakScores', names(red.df))])
     red.df[grep('^peakScores', names(red.df))] <- NULL
